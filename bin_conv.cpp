ebba8a84e5ad232040edcecbc90e9cc2a42a17e4
Jason Thatcher <jasont14@gmail.com>
2018-10-05 13:30:07 -0500

    Initial Binary Conversion

---
+++ bin_conv.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int main(){
+    int value;
+    std::cout<<"Enter an integer value between 0...1023";
+    std::cin>>value;
+
+    if (value => 0 && value < 1024){
+        std::cout<<value/512;
+        value %= 512;
+
+        std::cout<<value/256;
+        value %= 265;
+
+        std::cout<<value/128;
+        value %= 128;
+
+        std::cout<<value/64;
+        value %= 64;
+
+        std::cout<<value/32;
+        value %= 32;
+
+        std::cout<<value/16;
+        value %=16;
+
+        std::cout<<value/8;
+        value %= 8;
+
+        std::cout<<value/4;
+        value %=4;
+
+        std::cout<<value/2;
+        value %= 2;
+
+        std::cout << value << std::endl;
+    }
+}
